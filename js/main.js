0c4208e2a8116bbcb73db32df3d79bc5057a67ae
anhducne99 <trananhduc106999@gmail.com>
2026-05-26 18:25:01 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -194,31 +194,16 @@
 	
 	};
 
+	// Loading Page
+	setTimeout(() => {
+		document.getElementById("loading")
+		.classList.add("open");
+	}, 2000);
 
-	// Loading page
-	var loaderPage = function() {
-		$(".ducquynh-loader").fadeOut("slow");
-	};
-
-	var counter = function() {
-		$('.js-counter').countTo({
-			 formatter: function (value, options) {
-	      return value.toFixed(options.decimals);
-	    },
-		});
-	};
-
-	var counterWayPoint = function() {
-		if ($('#ducquynh-counter').length > 0 ) {
-			$('#ducquynh-counter').waypoint( function( direction ) {
-										
-				if( direction === 'down' && !$(this.element).hasClass('animated') ) {
-					setTimeout( counter , 400);					
-					$(this.element).addClass('animated');
-				}
-			} , { offset: '90%' } );
-		}
-	};
+	setTimeout(() => {
+		document.getElementById("loading")
+		.style.display = "none";
+	}, 4500);
 
 	// Parallax
 	var parallax = function() {
